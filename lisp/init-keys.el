1f45313c7803a830d2f503210f887e1875ad57ec
chichariotlu <luhancheng@baidu.com>
2016-03-24 17:01:43 +0800

    select n-th previous windowns

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -1,7 +1,17 @@
-;;indent whole buffer
+;; indent whole buffer
 (defun indent-whole-buffer ()
   "indent the whole buffer"
   (interactive)
   (save-excursion
-  (indent-region (point-min) (point-max)))
-(global-set-key [f7] 'indent-whole-buffer)
+    (indent-region (point-min) (point-max)))
+
+;; previous windows
+  (defun other-window-backward (&optional n)
+      "select the previsous window"
+      (interactive "P")
+      (other-window (- (or prefix-numeric-value 1))))
+
+;; global keys
+  (global-set-key [f7] 'indent-whole-buffer)
+  (global-set-key "\C-x\C-n" 'other-window)
+  (global-set-key "\C-x\C-p" 'other-window-backward)
