d8beabc7855182a9f7a85711e4e3d2473866fd6e
chichariotlu <luhancheng@baidu.com>
2016-03-25 17:05:06 +0800

    point to  top and buttom

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -4,12 +4,28 @@
   (interactive)
   (save-excursion
     (indent-region (point-min) (point-max)))
+  )
 
 ;; previous windows
-  (defun other-window-backward (&optional n)
-      "select the previsous window"
-      (interactive "P")
-      (other-window (- (or prefix-numeric-value 1))))
+(defun other-window-backward (&optional n)
+  "select the previsous window"
+  (interactive "P")
+  (other-window (- (or prefix-numeric-value 1)))
+  )
+
+;; point to top
+(defun point-to-top ()
+  "put cursor one top line of window"
+  (interactive)
+  (move-to-window-line 0)
+  )
+
+;; point to buttom
+(defun point-to-buttom ()
+  "put cursor on last visible line"
+  (interactive)
+  (move-to-window-line -1)
+  )
 
 ;; global keys
   (global-set-key [f7] 'indent-whole-buffer)
