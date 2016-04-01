75d416c7dbc06493270a61d96848c0f945476945
chichariotlu <luhancheng@baidu.com>
2016-04-01 18:42:02 +0800

    visittarget file instead

--- lisp/init-hooks.el
+++ lisp/init-hooks.el
@@ -1,6 +1,24 @@
-(def read-only-if-symlink ()
+;;  set read only if symlink file
+(add-hook 'find-file-hooks  '(lambda ()
      (if (file-symlink-p buffer-file-name)
-         (pron
+         (progn
           (setq buffer-read-only t)
-          (message "File is a symlink")))
-     )
+          (message "File is a symlink"))))
+          )
+
+(defun visit-target-instead
+    (interactive)
+  (if buffer-file-name
+      (let ((target (file-symlink-p buffer-file-name)))
+        (if target
+            (find-alternate-file target)
+          (error "Not visting a file")))
+          (error "Not visiting a file"))
+  )
+          
+    
+
+
+
+;;
+
